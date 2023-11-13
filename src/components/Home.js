a300de5fc58e0ae8bd708bb570589087ddcb1af4
Gathukumi <gathukumidavid001@gmail.com>
2023-11-13 14:21:30 +0300

    completed lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,30 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+// App.js
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
 
-function Home({ isLoggedIn }) {
-  if (!isLoggedIn) return <Redirect to="/login" />;
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <div>
-      <h1>Home!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar setIsLoggedIn={setIsLoggedIn} />
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/login">
+            <Login onLogin={() => setIsLoggedIn(true)} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default Home;
+export default App;
